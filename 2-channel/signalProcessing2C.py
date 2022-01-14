c47baf2f58994cf4fb1e251033694b12e10a9541
Ani Michaud <animichaud3@gmail.com>
2022-01-14 09:32:45 -0600

    removed hard-coding option (no GUI)

--- 2-channel/signalProcessing2C.py
+++ 2-channel/signalProcessing2C.py
@@ -25,148 +25,127 @@ plotIndividualPeaks = False     #True = plots signal trace and peak picking for 
 fileNameIndex = -1              #Necessary for "compareFiles = True", identifies the group index in the filename.
 acfPeakProm = 0.1               #Minimum peak prominence to choose in an ACF, set 0-1. Larger values are more stringent. 
 baseDirectory = "/Users/bementmbp/Desktop/smallTest"      #Base directory for the GUI. Can hard code file path by commenting line 23 and uncommenting line 24. 
-graphicUserInterface = True
 
        
 '''*** Start GUI Window ***'''
-if graphicUserInterface == True:
 
-    '''GUI Window '''
-    #initiates Tk window
-    root = tk.Tk()
-    root.title('Select your options')
-    root.geometry('500x250')
+'''GUI Window '''
+#initiates Tk window
+root = tk.Tk()
+root.title('Select your options')
+root.geometry('500x250')
 
-    #sets number of columns in the main window
-    root.columnconfigure(0, weight=1)
-    root.columnconfigure(1, weight=1)
-    root.columnconfigure(2, weight=1)
+#sets number of columns in the main window
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(2, weight=1)
 
-    #defining variable types for the different widget fields
-    boxSizeVar = tk.IntVar()            #variable for box grid size
-    boxSizeVar.set(20)                  #set default value 
-    plotIndividualACFsVar = tk.BooleanVar()     #variable for plotting individual ACFs
-    plotIndividualCCFsVar = tk.BooleanVar()     #variable for plotting individual CCFs
-    plotIndividualPeaksVar = tk.BooleanVar()    #variable for plotting individual peaks
-    acfPeakPromVar = tk.DoubleVar()             #variable for peak prominance threshold   
-    acfPeakPromVar.set(0.1)                     #set default value
-    groupNamesVar = tk.StringVar()   #variable for group names list
-    folderPath = tk.StringVar()      #variable for path to images
+#defining variable types for the different widget fields
+boxSizeVar = tk.IntVar()            #variable for box grid size
+boxSizeVar.set(20)                  #set default value 
+plotIndividualACFsVar = tk.BooleanVar()     #variable for plotting individual ACFs
+plotIndividualCCFsVar = tk.BooleanVar()     #variable for plotting individual CCFs
+plotIndividualPeaksVar = tk.BooleanVar()    #variable for plotting individual peaks
+acfPeakPromVar = tk.DoubleVar()             #variable for peak prominance threshold   
+acfPeakPromVar.set(0.1)                     #set default value
+groupNamesVar = tk.StringVar()   #variable for group names list
+folderPath = tk.StringVar()      #variable for path to images
 
-    #function for getting path to user's directory
-    def getFolderPath():
-        folderSelected = askdirectory()
-        folderPath.set(folderSelected)
-
+#function for getting path to user's directory
+def getFolderPath():
+    folderSelected = askdirectory()
+    folderPath.set(folderSelected)
     #function for hitting cancel button
-    def on_quit(): 
-        root.destroy() #destroys window
-        sys.exit("You opted to cancel the script!")
 
-    def on_start(): 
+def on_quit(): 
+    root.destroy() #destroys window
+    sys.exit("You opted to cancel the script!")
+
+
+def on_start(): 
         root.destroy() #destroys window
     
 
-    '''widget creation'''
-    #file path selection widget
-    fileEntry = ttk.Entry(root, textvariable=folderPath)
-    fileEntry.grid(column=0, row=0, padx=10, sticky='E')
-    browseButton = ttk.Button(root, text= 'Select source directory', command=getFolderPath)
-    browseButton.grid(column=1, row=0, sticky='W')
+'''widget creation'''
+#file path selection widget
+fileEntry = ttk.Entry(root, textvariable=folderPath)
+fileEntry.grid(column=0, row=0, padx=10, sticky='E')
+browseButton = ttk.Button(root, text= 'Select source directory', command=getFolderPath)
+browseButton.grid(column=1, row=0, sticky='W')
 
-    #boxSize entry widget
-    boxSizeBox = ttk.Entry(root, width = 3, textvariable=boxSizeVar) #creates box widget
-    boxSizeBox.grid(column=0, row=1, padx=10, sticky='E') #places widget in frame
-    boxSizeBox.focus()      #focuses cursor in box
-    boxSizeBox.icursor(2)   #positions cursor after default input characters
-    ttk.Label(root, text='Enter grid box size (px)').grid(column=1, row=1, columnspan=2, padx=10, sticky='W') #create label text
+#boxSize entry widget
+boxSizeBox = ttk.Entry(root, width = 3, textvariable=boxSizeVar) #creates box widget
+boxSizeBox.grid(column=0, row=1, padx=10, sticky='E') #places widget in frame
+boxSizeBox.focus()      #focuses cursor in box
+boxSizeBox.icursor(2)   #positions cursor after default input characters
+ttk.Label(root, text='Enter grid box size (px)').grid(column=1, row=1, columnspan=2, padx=10, sticky='W') #create label text
 
-    #create acfpeakprom entry widget
-    ttk.Entry(root, width = 3, textvariable=acfPeakPromVar).grid(column=0, row=2, padx=10, sticky='E') #create the widget
-    ttk.Label(root, text='Enter ACF peak prominence threshold').grid(column=1, row=2, padx=10, sticky='W') #create label text
+#create acfpeakprom entry widget
+ttk.Entry(root, width = 3, textvariable=acfPeakPromVar).grid(column=0, row=2, padx=10, sticky='E') #create the widget
+ttk.Label(root, text='Enter ACF peak prominence threshold').grid(column=1, row=2, padx=10, sticky='W') #create label text
 
-    #create groupNames entry widget
-    ttk.Entry(root,textvariable=groupNamesVar).grid(column=0, row=3, padx=10, sticky='E') #create the widget
-    ttk.Label(root, text='Enter group names separated by commas').grid(column=1, row=3, padx=10, sticky='W') #create label text
+#create groupNames entry widget
+ttk.Entry(root,textvariable=groupNamesVar).grid(column=0, row=3, padx=10, sticky='E') #create the widget
+ttk.Label(root, text='Enter group names separated by commas').grid(column=1, row=3, padx=10, sticky='W') #create label text
 
-    #create checkbox widgets and labels
-    ttk.Checkbutton(root, variable=plotIndividualACFsVar).grid(column=0, row=5, sticky='E', padx=15)
-    ttk.Label(root, text='Plot individual ACFs').grid(column=1, row=5, columnspan=2, padx=10, sticky='W') #plot individual ACFs
+#create checkbox widgets and labels
+ttk.Checkbutton(root, variable=plotIndividualACFsVar).grid(column=0, row=5, sticky='E', padx=15)
+ttk.Label(root, text='Plot individual ACFs').grid(column=1, row=5, columnspan=2, padx=10, sticky='W') #plot individual ACFs
+ttk.Checkbutton(root, variable=plotIndividualCCFsVar).grid(column=0, row=6, sticky='E', padx=15) #plot individual CCFs
+ttk.Label(root, text='Plot individual CCFs').grid(column=1, row=6, columnspan=2, padx=10, sticky='W')
 
-    ttk.Checkbutton(root, variable=plotIndividualCCFsVar).grid(column=0, row=6, sticky='E', padx=15) #plot individual CCFs
-    ttk.Label(root, text='Plot individual CCFs').grid(column=1, row=6, columnspan=2, padx=10, sticky='W')
+ttk.Checkbutton(root, variable=plotIndividualPeaksVar).grid(column=0, row=7, sticky='E', padx=15) #plot individual peaks
+ttk.Label(root, text='Plot individual peaks').grid(column=1, row=7, columnspan=2, padx=10, sticky='W')
 
-    ttk.Checkbutton(root, variable=plotIndividualPeaksVar).grid(column=0, row=7, sticky='E', padx=15) #plot individual peaks
-    ttk.Label(root, text='Plot individual peaks').grid(column=1, row=7, columnspan=2, padx=10, sticky='W')
+#Creates the 'Start Analysis' button
+startButton = ttk.Button(root, text='Start Analysis', command=on_start) #creates the button and bind it to close the window when clicked
+startButton.grid(column=1, row=9, pady=10, sticky='W') #place it in the tk window
 
-    #Creates the 'Start Analysis' button
-    startButton = ttk.Button(root, text='Start Analysis', command=on_start) #creates the button and bind it to close the window when clicked
-    startButton.grid(column=1, row=9, pady=10, sticky='W') #place it in the tk window
+#Creates the 'Cancel' button
+cancelButton = ttk.Button(root, text='Cancel', command=on_quit) #creates the button and bind it to on_quit function
+cancelButton.grid(column=0, row=9, pady=10, sticky='E') #place it in the tk window
 
-    #Creates the 'Cancel' button
-    cancelButton = ttk.Button(root, text='Cancel', command=on_quit) #creates the button and bind it to on_quit function
-    cancelButton.grid(column=0, row=9, pady=10, sticky='E') #place it in the tk window
-    
-    root.protocol("WM_DELETE_WINDOW", on_quit) #calls on_quit if the root window is x'd out.
-    root.mainloop() #run the script
+root.protocol("WM_DELETE_WINDOW", on_quit) #calls on_quit if the root window is x'd out.
+root.mainloop() #run the script
 
-    #get the values stored in the widget
-    boxSizeInPx = boxSizeVar.get()
-    plotIndividualACFs= plotIndividualACFsVar.get()
-    plotIndividualCCFs = plotIndividualCCFsVar.get()
-    plotIndividualPeaks = plotIndividualPeaksVar.get()
-    acfPeakProm = acfPeakPromVar.get()
-    groupNames = groupNamesVar.get()
-    groupNames = [x.strip() for x in groupNames.split(',')] #list of group names. splits string input by commans and removes spaces
-    baseDirectory = folderPath.get() 
+#get the values stored in the widget
+boxSizeInPx = boxSizeVar.get()
+plotIndividualACFs= plotIndividualACFsVar.get()
+plotIndividualCCFs = plotIndividualCCFsVar.get()
+plotIndividualPeaks = plotIndividualPeaksVar.get()
+acfPeakProm = acfPeakPromVar.get()
+groupNames = groupNamesVar.get()
+groupNames = [x.strip() for x in groupNames.split(',')] #list of group names. splits string input by commans and removes spaces
+baseDirectory = folderPath.get() 
 
-    #make dictionary of parameters for log file use
-    logParams = {
-        "Box Size(px)" : boxSizeInPx,
-        "Base Directory" : baseDirectory,
-        "ACF Peak Prominence" : acfPeakProm,
-        "Group Names" : groupNames,
-        "Plot Individual ACFs" : plotIndividualACFs,
-        "Plot Individual CCFs" : plotIndividualCCFs,
-        }
-
-    errors = []
-    errorMessage = False
-    if acfPeakProm > 1 :
-        errorMessage = True
-        errors.append("The ACF peak prominence can not be greater than 1, set 'ACF peak prominence threshold' to a value between 0 and 1. More realistically, a value between 0 and 0.5")
-    if len(baseDirectory) < 1 :
-        errorMessage = True
-        errors.append("You didn't enter a directory to analyze")
-
-
-    if errorMessage == True:
-        if len(errors) == 1 :
-            print(errors[0])
-            sys.exit("It's okay! Just fix that error and try again")
-        else:
-            for error in enumerate(errors):
-                print(str(error[0]+1) + ') ' + error[1])
-            sys.exit("It's okay! Just fix those errors and try again")
-
-elif graphicUserInterface != True:
-    logParams = {
+#make dictionary of parameters for log file use
+logParams = {
     "Box Size(px)" : boxSizeInPx,
     "Base Directory" : baseDirectory,
-    "ACF Peak Prominence"  : acfPeakProm,
-
-    "Plot Individual Peaks": plotIndividualPeaks,
+    "ACF Peak Prominence" : acfPeakProm,
+    "Group Names" : groupNames,
     "Plot Individual ACFs" : plotIndividualACFs,
     "Plot Individual CCFs" : plotIndividualCCFs,
     }
-    fileNames = [fname for fname in os.listdir(baseDirectory) if fname.endswith('.tif')]
-    groupNames = []
-    for fileName in fileNames:
-        extensionless = fileName.rsplit(".",1)[0]
-        group = extensionless.split("_")[fileNameIndex]
-        if group not in groupNames:
-            groupNames.append(group)
-    logParams["Group Names"] = groupNames
+
+errors = []
+errorMessage = False
+if acfPeakProm > 1 :
+    errorMessage = True
+    errors.append("The ACF peak prominence can not be greater than 1, set 'ACF peak prominence threshold' to a value between 0 and 1. More realistically, a value between 0 and 0.5")
+if len(baseDirectory) < 1 :
+    errorMessage = True
+    errors.append("You didn't enter a directory to analyze")
+
+
+if errorMessage == True:
+    if len(errors) == 1 :
+        print(errors[0])
+        sys.exit("It's okay! Just fix that error and try again")
+    else:
+        for error in enumerate(errors):
+            print(str(error[0]+1) + ') ' + error[1])
+        sys.exit("It's okay! Just fix those errors and try again")
 
 '''*** End GUI Window ***'''
 
