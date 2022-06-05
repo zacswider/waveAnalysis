eb2d5e1d6df7ee6ac1f72868fef727f682ca9d8f
Zac Swider <51267747+zacswider@users.noreply.github.com>
2022-06-05 16:12:37 -0500

    Update custom_gui.py
    added test datasets as a default for troubleshooting

--- 2-channel/custom_gui.py
+++ 2-channel/custom_gui.py
@@ -31,6 +31,8 @@ class CustomGUI(tk.Tk):
         self.file_path_entry = ttk.Entry(self, textvariable = self.folder_path)
         self.file_path_entry.grid(row = 0, column = 0, padx = 10, sticky = 'E')
         self.file_path_button = ttk.Button(self, text = 'Select folder')
+        # make a default path
+        self.folder_path.set('/Users/bementmbp/Desktop/Scripts/waveAnalysis/2-channel/testDatasets/')
         self.file_path_button['command'] = self.get_folder_path
         self.file_path_button.grid(row = 0, column = 1, padx = 10, sticky = 'W')        
 
