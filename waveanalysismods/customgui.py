c946f1a507033996633f90d776cfe6f51bb3da9c
Zac Swider <51267747+zacswider@users.noreply.github.com>
2022-06-14 19:11:07 -0500

    Update customgui.py
    added option for individual peak plots

--- waveanalysismods/customgui.py
+++ waveanalysismods/customgui.py
@@ -20,11 +20,14 @@ class BaseGUI(tk.Tk):
         self.box_size = tk.IntVar()
         self.box_size.set(20)
         self.box_shift = tk.IntVar()
-        self.box_shift.set(20)
+        self.box_shift.set(30)
         self.plot_summary_CFs = tk.BooleanVar()
-        self.plot_summary_CFs.set(True)
+        #self.plot_summary_CFs.set(True)
         self.plot_summary_peaks = tk.BooleanVar()
-        self.plot_summary_peaks.set(True)
+        #self.plot_summary_peaks.set(True)
+        self.plot_ind_CFs = tk.BooleanVar()
+        self.plot_ind_peaks = tk.BooleanVar()
+        self.plot_ind_peaks.set(True)
         self.acf_peak_thresh = tk.DoubleVar()
         self.acf_peak_thresh.set(0.1)
         self.group_names = tk.StringVar()
@@ -37,6 +40,7 @@ class BaseGUI(tk.Tk):
         self.file_path_entry.grid(row = 0, column = 0, padx = 10, sticky = 'E')
         self.file_path_button = ttk.Button(self, text = 'Select folder')
         # make a default path
+        self.folder_path.set('/Users/bementmbp/Desktop/Scripts/waveAnalysis/small_test')
         self.file_path_button['command'] = self.get_folder_path
         self.file_path_button.grid(row = 0, column = 1, padx = 10, sticky = 'W')        
 
@@ -67,32 +71,44 @@ class BaseGUI(tk.Tk):
         self.group_names_label = ttk.Label(self, text = 'Group names')
         self.group_names_label.grid(row = 4, column = 1, padx = 10, sticky = 'W')
 
-        # create checkbox for plotting individual CFs
+        # create checkbox for plotting summary CFs
         self.plot_summary_CFs_checkbox = ttk.Checkbutton(self, variable = self.plot_summary_CFs)
         self.plot_summary_CFs_checkbox.grid(row = 5, column = 0, padx = 10, sticky = 'E')
         self.plot_summary_CFs_label = ttk.Label(self, text = 'Plot summary CFs')
         self.plot_summary_CFs_label.grid(row = 5, column = 1, padx = 10, sticky = 'W')
 
-        # create checkbox for plotting individual peaks
+        # create checkbox for plotting summary peaks
         self.plot_summary_peaks_checkbox = ttk.Checkbutton(self, variable = self.plot_summary_peaks)
         self.plot_summary_peaks_checkbox.grid(row = 6, column = 0, padx = 10, sticky = 'E')
         self.plot_summary_peaks_label = ttk.Label(self, text = 'Plot summary peaks')
         self.plot_summary_peaks_label.grid(row = 6, column = 1, padx = 10, sticky = 'W')
+
+        # create checkbox for plotting individual CFs
+        self.plot_ind_CFs_checkbox = ttk.Checkbutton(self, variable = self.plot_ind_CFs)
+        self.plot_ind_CFs_checkbox.grid(row = 7, column = 0, padx = 10, sticky = 'E')
+        self.plot_ind_CFs_label = ttk.Label(self, text = 'Plot individual CFs')
+        self.plot_ind_CFs_label.grid(row = 7, column = 1, padx = 10, sticky = 'W')
+
+        # create checkbox for plotting individual peaks
+        self.plot_ind_peaks_checkbox = ttk.Checkbutton(self, variable = self.plot_ind_peaks)
+        self.plot_ind_peaks_checkbox.grid(row = 8, column = 0, padx = 10, sticky = 'E')
+        self.plot_ind_peaks_label = ttk.Label(self, text = 'Plot individual peaks')
+        self.plot_ind_peaks_label.grid(row = 8, column = 1, padx = 10, sticky = 'W')
         
         # create start button
         self.start_button = ttk.Button(self, text = 'Start analysis')
         self.start_button['command'] = self.start_analysis
-        self.start_button.grid(row = 7, column = 0, padx = 10, sticky = 'E')
+        self.start_button.grid(row = 9, column = 0, padx = 10, sticky = 'E')
 
         # create cancel button
         self.cancel_button = ttk.Button(self, text = 'Cancel')
         self.cancel_button['command'] = self.cancel_analysis
-        self.cancel_button.grid(row = 7, column = 1, padx = 10, sticky = 'W')
+        self.cancel_button.grid(row = 9, column = 1, padx = 10, sticky = 'W')
 
         # create button to launch rolling analysis gui
         self.rolling_button = ttk.Button(self, text = 'Launch rolling analysis')
         self.rolling_button['command'] = self.launch_rolling_analysis
-        self.rolling_button.grid(row = 8, column = 0, padx = 10, sticky = 'E')
+        self.rolling_button.grid(row = 10, column = 0, padx = 10, sticky = 'E')
 
     def get_folder_path(self):
         self.folder_path.set(askdirectory())
@@ -112,6 +128,8 @@ class BaseGUI(tk.Tk):
         self.group_names = self.group_names.get()
         self.plot_summary_CFs = self.plot_summary_CFs.get()
         self.plot_summary_peaks = self.plot_summary_peaks.get()
+        self.plot_ind_CFs = self.plot_ind_CFs.get()
+        self.plot_ind_peaks = self.plot_ind_peaks.get()
         self.folder_path = self.folder_path.get()
         
         # convert group names to list of strings
