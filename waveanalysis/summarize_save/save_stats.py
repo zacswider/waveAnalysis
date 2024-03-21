489d6e50cee98261e93a67f228187f2ca390ac95
domchom <67873793+domchom@users.noreply.github.com>
2024-03-21 16:24:00 -0500

    More comments and docstrings

--- waveanalysis/summarize_save/save_stats.py
+++ waveanalysis/summarize_save/save_stats.py
@@ -10,6 +10,17 @@ def save_parameter_means_to_csv(
     summary_df: pd.DataFrame,
     group_names: list,
 ) -> dict:
+    '''
+    Save the mean values of parameters for each group to separate CSV files.
+
+    Parameters:
+        summary_df (pd.DataFrame): The summary dataframe containing the parameter values.
+        group_names (list): A list of group names.
+
+    Returns:
+        dict: A dictionary containing the filenames as keys and the corresponding parameter tables as values.
+    '''
+    # create a dictionary to store the parameter tables
     parameter_tables_dict = {}
     parameters_to_extract = [column for column in summary_df.columns if 'Mean' in column]
 
@@ -24,6 +35,7 @@ def save_parameter_means_to_csv(
             group_data = summary_df.loc[summary_df['File Name'].str.contains(group_name)]
             values = group_data[parameter].tolist()
             df_to_concat = pd.DataFrame({'Data Type': parameter, 'Group Name': group_name, 'Value': values})
+            # concatenate the dataframes
             if not individual_parameter_table.empty:
                 individual_parameter_table = pd.concat([individual_parameter_table, df_to_concat], ignore_index=True)
             else:
@@ -49,12 +61,28 @@ def get_mean_CCF_values(
     indv_ccfs: np.ndarray, 
     frame_interval: float
 ) -> dict:
+    '''
+    Calculate the mean cross-correlation function (CCF) values for each channel combination.
 
+    Args:
+        channel_combos (list): A list of channel combinations.
+        indv_ccfs (np.ndarray): An array of individual CCFs.
+        frame_interval (float): The time interval between frames.
+
+    Returns:
+        dict: A dictionary containing the mean CCF values for each channel combination.
+              The keys are in the format 'ChX-ChY Mean CCF values', where X and Y are the channel numbers.
+              The values are lists of tuples, where each tuple contains the frame index, mean CCF value, and standard deviation.
+    '''
+    # Initialize dictionary to store the mean CCF values
     mean_ccf_values = {}
 
+    # Loop through each channel combination
     for combo_number, combo in enumerate(channel_combos):
+        # Calculate the mean and standard deviation of the CCF values
         arr_mean = np.nanmean(indv_ccfs[combo_number], axis=0)
         arr_std = np.nanstd(indv_ccfs[combo_number], axis=0)
+        # Create a list of frame indices
         arr_list = [i * frame_interval for i in range(len(arr_mean))]
         # Combine mean and standard deviation into a list of tuples
         mean_ccf_values[f'Ch{combo[0] + 1}-Ch{combo[1] + 1} Mean CCF values'] = list(zip_longest(arr_list, arr_mean, arr_std, fillvalue=None))
@@ -66,19 +94,33 @@ def get_indv_CCF_values(
     bin_values:np.ndarray,
     img_props_dict: dict
 ) -> dict:
-    
+    '''
+    Calculate and return the individual cross-correlation function (CCF) values for each channel combination and bin.
+
+    Parameters:
+    - indv_ccfs (np.ndarray): Array of individual CCF curves for each channel combination and bin.
+    - bin_values (np.ndarray): Array of bin values for each channel combination and bin.
+    - img_props_dict (dict): Dictionary containing image properties such as frame interval, number of bins, analysis type, and channel combinations.
+
+    Returns:
+    - indv_ccf_values (dict): Dictionary containing the individual CCF values for each channel combination and bin.
+    '''
+    # Extract image properties from the dictionary
     frame_interval = img_props_dict['frame_interval']
     num_bins = img_props_dict['num_bins']
     analysis_type = img_props_dict['analysis_type']
     channel_combos = img_props_dict['channel_combos']
     
+    # Initialize dictionary to store the individual CCF values
     indv_ccf_values = {}
 
+    # Loop through each channel combination and bin
     for combo_number, combo in enumerate(channel_combos):
         for bin in range(num_bins):      
             # Save the individual bin values
             to_plot1 = bin_values[:, combo[0], bin] if analysis_type == "standard" else bin_values[combo[0], bin]
             to_plot2 = bin_values[:, combo[1], bin] if analysis_type == "standard" else bin_values[combo[1], bin]
+            # Create a list of tuples containing the time, channel 1 value, channel 2 value, and CCF value
             ccf_curve = indv_ccfs[combo_number, bin]
             arr_list = [i * frame_interval for i in range(len(ccf_curve))]
             measurements = list(zip_longest(arr_list,  normalize_signal(to_plot1), normalize_signal(to_plot2), ccf_curve, fillvalue=None))
@@ -90,13 +132,19 @@ def get_indv_CCF_values(
 def save_ccf_values_to_csv(
     values: dict, 
     path: str,
-):
+) -> None:
+    '''
+    Save the CCF values to CSV files.
+    '''
     for filename, measurements in values.items():
         file_path = os.path.join(path, f'{filename}.csv')
         headers, data = determine_structure_and_values(measurements)
         write_to_csv(file_path, headers, data)
 
 def determine_structure_and_values(measurements: Union[List[Tuple], List[List]]) -> Tuple[List[str], List[Tuple]]:
+    '''
+    Determine the structure of the measurements and return the headers and values.
+    '''
     # Check the structure of the measurements to determine headers and values
     first_entry = measurements[0]
     if len(first_entry) == 4:
@@ -110,7 +158,10 @@ def determine_structure_and_values(measurements: Union[List[Tuple], List[List]])
 
     return headers, measurements
 
-def write_to_csv(file_path: str, headers: List[str], data: List[Tuple]):
+def write_to_csv(file_path: str, headers: List[str], data: List[Tuple]) -> None:
+    '''
+    Write the headers and data to a CSV file.
+    '''
     with open(file_path, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(headers)
--- waveanalysis/summarize_save/summarize_kymo_standard.py
+++ waveanalysis/summarize_save/summarize_kymo_standard.py
@@ -5,7 +5,18 @@ def summarize_image_standard_kymo(
     img_parameters: dict,
     img_props_dict: dict
 ) -> pd.DataFrame:
-    
+    '''
+    Summarizes the image parameters and properties of a standard kymograph.
+
+    Args:
+        img_parameters (dict): A dictionary containing the image parameters.
+        img_props_dict (dict): A dictionary containing the image properties.
+
+    Returns:
+        pd.DataFrame: A dataframe summarizing the bin results.
+
+    '''
+    # Extract image properties from the dictionary
     num_bins = img_props_dict['num_bins']
     num_channels = img_props_dict['num_channels']
     channel_combos = img_props_dict['channel_combos']
@@ -20,19 +31,23 @@ def summarize_image_standard_kymo(
 
     # insert Mean, Median, StdDev, and SEM into the beginning of each list
     for key, value in img_parameters.items():
+        # Skip the Shift key since it is handled separately
         if key == 'Shift':
             continue
+        # Wave Speed is a single value, so it doesn't need to be separated by channel
         elif key == 'Wave Speed':
             parameter_with_stats = add_stats_for_parameter(img_parameters[key], key, num_channels, channel_combos)
             parameter_with_stats = sum(parameter_with_stats, []) # flatten the list
             parameter_with_stats_dict[key] = parameter_with_stats
             statified_measurements.append(parameter_with_stats)
         else:
+            # Add stats for each channel
             parameter_with_stats = add_stats_for_parameter(img_parameters[key], key, num_channels, channel_combos)
             parameter_with_stats_dict[key] = parameter_with_stats
             for channel in range(num_channels):
                 statified_measurements.append(parameter_with_stats[channel])
             
+    # Add stats for Shift
     if num_channels > 1:
         shifts_with_stats = add_stats_for_parameter(img_parameters['Shift'], 'Shift', num_channels, channel_combos)
         parameter_with_stats_dict['Shift'] = shifts_with_stats
@@ -49,36 +64,51 @@ def add_stats_for_parameter(
         num_channels: int, 
         channel_combos: list = None
 ) -> list:
-        
-        statified = []
+    '''
+    Calculate statistics for a given measurement parameter.
 
-        if measurement_name != 'Wave Speed':
-            for index, item in enumerate(channel_combos if measurement_name == 'Shift' else range(num_channels)):
-                if measurement_name == 'Shift':
-                    measurements_subset = measurements[index]
-                    channel_label = f'Ch{channel_combos[index][0]+1}-Ch{channel_combos[index][1]+1} {measurement_name}'
-                else:
-                    measurements_subset = measurements[item]
-                    channel_label = f'Ch {item + 1} {measurement_name}'
-                
-                meas_mean = np.nanmean(measurements_subset)
-                meas_median = np.nanmedian(measurements_subset)
-                meas_std = np.nanstd(measurements_subset)
-                meas_sem = meas_std / np.sqrt(len(measurements_subset))
-                meas_list = [channel_label, meas_mean, meas_median, meas_std, meas_sem]
-                meas_list.extend(measurements_subset.tolist())
-                statified.append(meas_list)
+    Parameters:
+        measurements (np.ndarray): Array of measurements.
+        measurement_name (str): Name of the measurement parameter.
+        num_channels (int): Number of channels.
+        channel_combos (list, optional): List of channel combinations. Defaults to None.
 
-        else:
-            meas_mean = np.nanmean(measurements)
-            meas_median = np.nanmedian(measurements)
-            meas_std = np.nanstd(measurements)
-            meas_sem = meas_std / np.sqrt(len(measurements))
-            meas_list = [measurement_name, meas_mean, meas_median, meas_std, meas_sem]
-            meas_list.extend(measurements)
-            statified.append(meas_list)
+    Returns:
+        list: List of statistics for the measurement parameter.
+    '''
+    # Initialize list to store the statified measurements
+    statified = []
+
+    # Calculate the mean, median, standard deviation, and standard error of the mean
+    if measurement_name != 'Wave Speed':
+        # If there are multiple channels, calculate the statistics for each channel combination
+        for index, item in enumerate(channel_combos if measurement_name == 'Shift' else range(num_channels)):
+            if measurement_name == 'Shift':
+                measurements_subset = measurements[index]
+                channel_label = f'Ch{channel_combos[index][0]+1}-Ch{channel_combos[index][1]+1} {measurement_name}'
+            else:
+                measurements_subset = measurements[item]
+                channel_label = f'Ch {item + 1} {measurement_name}'
             
-        return statified
+            meas_mean = np.nanmean(measurements_subset)
+            meas_median = np.nanmedian(measurements_subset)
+            meas_std = np.nanstd(measurements_subset)
+            meas_sem = meas_std / np.sqrt(len(measurements_subset))
+            meas_list = [channel_label, meas_mean, meas_median, meas_std, meas_sem]
+            meas_list.extend(measurements_subset.tolist())
+            statified.append(meas_list)
+
+    # Calculate the statistics for the wave speed
+    else:
+        meas_mean = np.nanmean(measurements)
+        meas_median = np.nanmedian(measurements)
+        meas_std = np.nanstd(measurements)
+        meas_sem = meas_std / np.sqrt(len(measurements))
+        meas_list = [measurement_name, meas_mean, meas_median, meas_std, meas_sem]
+        meas_list.extend(measurements)
+        statified.append(meas_list)
+        
+    return statified
 
 def combine_stats_for_image_kymo_standard(
     file_name: str, 
@@ -86,8 +116,21 @@ def combine_stats_for_image_kymo_standard(
     img_props: dict,
     img_parameters_dict: dict,
     parameters_with_stats_dict: dict
-    ) -> dict:
+) -> dict:
+    '''
+    Combine the statistics for an image in a kymograph or standard analysis.
 
+    Args:
+        file_name (str): The name of the file.
+        group_name (str): The name of the group.
+        img_props (dict): A dictionary containing image properties.
+        img_parameters_dict (dict): A dictionary containing image parameters.
+        parameters_with_stats_dict (dict): A dictionary containing parameters with statistics.
+
+    Returns:
+        dict: A dictionary containing the summarized measurements for each image.
+    '''
+    # Extract image properties from the dictionary
     num_bins = img_props['num_bins']
     num_channels = img_props['num_channels']
     channel_combos = img_props['channel_combos']
@@ -98,8 +141,10 @@ def combine_stats_for_image_kymo_standard(
     file_data_summary['Group Name'] = group_name if group_name else 'None'
     file_data_summary['Num Bins'] = num_bins
 
+    # column names for the dataframe summarizing the bin results
     stats_location = ['Mean', 'Median', 'StdDev', 'SEM']
 
+    # Add stats for each Shifts
     if num_channels > 1:
         for combo_number, combo in enumerate(channel_combos):
             shift_data = img_parameters_dict['Shift'][combo_number]
@@ -108,9 +153,12 @@ def combine_stats_for_image_kymo_standard(
             for ind, stat in enumerate(stats_location):
                 file_data_summary[f'Ch{combo[0] + 1}-Ch{combo[1] + 1} {stat} Shift'] = parameters_with_stats_dict['Shift'][combo_number][ind + 1]
 
+    # Add stats for each parameter
     for key, value in img_parameters_dict.items():
+        # Skip the Shift key since it is handled separately
         if key == 'Shift':
             continue
+        # We calculate the number of bins without Period and Peak Amp 
         elif key == 'Period' or key == 'Peak Amp':
             for channel in range(num_channels):
                 pcnt_no_parameter = np.count_nonzero(np.isnan(img_parameters_dict[key][channel])) / img_parameters_dict[key][channel].shape[0] * 100
@@ -118,10 +166,12 @@ def combine_stats_for_image_kymo_standard(
                 file_data_summary[f'Ch {channel + 1} Pcnt No {param}'] = pcnt_no_parameter
                 for ind, stat in enumerate(stats_location):
                     file_data_summary[f'Ch {channel + 1} {stat} {key}'] = parameters_with_stats_dict[key][channel][ind + 1]
+        # Wave Speed is a single value, so it doesn't need to be separated by channel
         elif key != 'Wave Speed':
             for channel in range(num_channels):        
                 for ind, stat in enumerate(stats_location):
                     file_data_summary[f'Ch {channel + 1} {stat} {key}'] = parameters_with_stats_dict[key][channel][ind + 1]
+        # other parameters
         elif key == 'Wave Speed':
             for ind, stat in enumerate(stats_location):
                 file_data_summary[f'{stat} {key}'] = parameters_with_stats_dict[key][ind + 1]
