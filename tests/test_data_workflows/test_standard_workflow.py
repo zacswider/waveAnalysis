1cf207531fef10107c7080fb0b9de039d5efc541
domchom <67873793+domchom@users.noreply.github.com>
2024-03-25 14:53:53 -0500

    Update test_standard_workflow.py

--- tests/test_data_workflows/test_standard_workflow.py
+++ tests/test_data_workflows/test_standard_workflow.py
@@ -32,7 +32,7 @@ def test_standard_workflow(default_log_params):
     assert isinstance(known_results, pd.DataFrame)
     exp_results = combined_workflow(
         main_directory=str(Path('tests/assets/standard/')),
-        group_names=['Group1','Group2'],
+        group_names=['Group1, Group2'],
         log_params=default_log_params,
         analysis_type='standard',
         box_size=default_log_params['Box Size(px)'],
