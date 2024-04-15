4e9fb497c7da3e5f884d1340031863b206e7c987
domchom <67873793+domchom@users.noreply.github.com>
2024-04-15 14:15:31 -0500

    Update __init__.py

--- waveanalysis/signal_processing/__init__.py
+++ waveanalysis/signal_processing/__init__.py
@@ -10,7 +10,6 @@ from .correlation_functions import (
     calc_indv_CCF
 )
 from .peak_properties import calc_indv_peak_props_workflow, calc_indv_peak_props_rolling
-from .wave_speed import define_wave_tracks, calc_wave_speeds
 
 __all__ = [
     "calc_indv_ACF_workflow",
@@ -18,8 +17,6 @@ __all__ = [
     "calc_indv_period_workflow",
     "calc_indv_shift_workflow",
     "calc_indv_peak_props_workflow",
-    'define_wave_tracks',
-    'calc_wave_speeds',
     'calc_indv_ACF',
     'calc_indv_period',
     'calc_indv_shift',
