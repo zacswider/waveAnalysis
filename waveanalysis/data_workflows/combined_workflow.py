16778630b85346784d2e0b7da90b49314315f813
domchom <67873793+domchom@users.noreply.github.com>
2024-04-19 14:42:30 -0500

    compatibilty

--- waveanalysis/data_workflows/combined_workflow.py
+++ waveanalysis/data_workflows/combined_workflow.py
@@ -5,15 +5,15 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from typing import Any
-import waveanalysis.plotting as pt
-import waveanalysis.signal_processing as sp
-import waveanalysis.housekeeping.housekeeping_functions as hf 
+import plotting as pt
+import signal_processing as sp
+import housekeeping.housekeeping_functions as hf 
 
-from waveanalysis.image_props.image_bin_calc import create_multi_frame_bin_array, create_kymo_bin_array
-from waveanalysis.image_props.image_to_np_arrays import tiff_to_np_array_multi_frame, tiff_to_np_array_single_frame
-from waveanalysis.image_props.image_properties import get_multi_frame_properties, get_single_frame_properties
-from waveanalysis.summarize_save.save_stats import save_parameter_means_to_csv, get_mean_CCF_values, get_indv_CCF_values, save_ccf_values_to_csv
-from waveanalysis.summarize_save.summarize_images import summarize_image, combine_stats_for_image_kymo_standard
+from image_props.image_bin_calc import create_multi_frame_bin_array, create_kymo_bin_array
+from image_props.image_to_np_arrays import tiff_to_np_array_multi_frame, tiff_to_np_array_single_frame
+from image_props.image_properties import get_multi_frame_properties, get_single_frame_properties
+from summarize_save.save_stats import save_parameter_means_to_csv, get_mean_CCF_values, get_indv_CCF_values, save_ccf_values_to_csv
+from summarize_save.summarize_images import summarize_image, combine_stats_for_image_kymo_standard
 
 def combined_workflow(
     folder_path: str,
